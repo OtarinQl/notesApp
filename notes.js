ec82978759b5c83e7a60ab03c4a0d5ccae92d941
OtarinQl <44878608+OtarinQl@users.noreply.github.com>
2019-07-05 19:54:08 -0300

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs') 
+const chalk = require('chalk')
 
 const showNotes = ()=>{
     //Va a retornar una lista de notas en JSON.
@@ -20,15 +21,48 @@ const getNotes = ()=>{
 
 const addNote = (title,body)=>{
     const noteList = showNotes()
-    noteList.push({
-        title: title,
-        body: body
-    })
-    fs.writeFileSync('data.json',JSON.stringify(noteList))
-    console.log('Saved note ' + title + '.')
+    const secondList = noteList.find((note)=>{return note.title === title})
+    
+    if(secondList === undefined){
+        noteList.push({
+            title: title,
+            body: body
+        })
+        fs.writeFileSync('data.json',JSON.stringify(noteList))
+        console.log(chalk.bgGreen.white.bold('Nota ' + title + ' guardada.'))
+    } else {
+        console.log(chalk.bgRed.white.bold('Ya existe una nota llamada así.'))
+    }
+    
 }
+
+const removeNote = (title)=>{
+    var noteList = showNotes()
+    const searcher = noteList.find((note)=>{return note.title === title})
+    
+    if(searcher === undefined){
+        console.log(chalk.bgCyan.white.bold('No se ha encontrado la nota.'))
+    } else {
+        noteList.splice(noteList.indexOf(searcher),1)
+        fs.writeFileSync('data.json',JSON.stringify(noteList))
+        console.log(chalk.bgBlue.white.bold('Se ha eliminado con éxito.'))
+    }
+}
+
+const listNotes = ()=>{
+    const noteList = showNotes()
+    if(noteList.length === 0){
+        console.log(chalk.bgRed.bold('You have no notes.')) 
+    } else {
+        console.log(chalk.bold.bgBlue('Your notes are:'))
+        noteList.forEach((note)=>{console.log(chalk.bgGreen.bold(note.title + ': ' + note.body))})
+    }
+}
+
 
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes
 }
